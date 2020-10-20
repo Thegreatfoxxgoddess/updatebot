e7ada338158593fa9669f8d7e67d86f08cc16dbb
RoyalBoy69 <64057443+RoyalBoy69@users.noreply.github.com>
2020-10-20 18:20:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ fi
 echo " "
 echo "Updating your oub-remix"
 git pull https://github.com/sahyam2019/oub-remix.git
-git commit -m "😎"
+git commit -m "termux merge"
 git push
 echo " "
 echo "Updated"
